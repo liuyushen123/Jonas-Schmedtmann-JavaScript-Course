f217ba7ba0a809b76c93805bd275c9a3cffb3901
Liu yuchen <liuyuchen@Esdeathe-4.local>
2026-04-28 17:43:45 -0500

    Add portfolio project and Section 11 challenge

---
+++ Section 11/Challenge-3/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const data1 = [10, 20, 3, 23, 24, 25, 26];
+
+const calcAverageHumanAge = (ages) =>
+  ages
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age) => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+console.log(calcAverageHumanAge(data1));
